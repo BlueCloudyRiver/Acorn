cc85bc40e4a97b83a6494958f42fbba1a21e0968
River <bluecloudyriver@gmail.com>
2026-05-28 18:31:07 +0530

    Implement robot state management and battery logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+#[derive(Debug)]
+enum RobotState {
+    Idle,
+    Moving, 
+    Charging,
+}
+
 pub struct Robot {
     pub id: u32,
     pub name: String,
     pub x: i32,
     pub y: i32,
     pub battery: i32,
+    pub state: RobotState,
 }
 
 pub struct World {
@@ -13,7 +21,31 @@ pub struct World {
 impl World {
     pub fn tick(&mut self) {
         for robot in &mut self.robots {
-            robot.battery -= 1;
+            match robot.state {
+                RobotState::Charging => {
+                    robot.battery += 3;
+
+                    if robot.battery >= 100 {
+                        robot.battery = 100;
+                        robot.state = RobotState::Idle;
+                    }
+                }
+                RobotState::Moving => {
+                    robot.battery -= 2;
+                    robot.x += 1;
+
+                    if robot.battery <= 20 {
+                        robot.state = RobotState::Charging;
+                    }
+                }
+                RobotState::Idle => {
+                    robot.battery -= 1;
+
+                    if robot.battery <= 50 {
+                        robot.state = RobotState::Moving;
+                    }
+                }
+            }
         }
     }
 }
@@ -27,12 +59,25 @@ fn main() {
                 x: 0,
                 y: 0,
                 battery: 100,
+                state: RobotState::Charging,
             }
         ],
     };
 
     for tick in 0..5 {
-        println!("Tick {}", tick);
+        println!("--- Tick {} ---", tick);
+
+        for robot in &world.robots {
+            println!(
+                "{} | {:?} | battery: {} | position: ({}, {})", 
+                robot.name,
+                robot.state,
+                robot.battery,
+                robot.x,
+                robot.y
+            );
+        }
+
         world.tick();
     }
 }
