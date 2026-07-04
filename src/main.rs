fa6e795a4f0de85e9c7210902969b65a41396df4
BlueCloudyRiver <bluecloudyriver@gmail.com>
2026-07-04 10:19:01 +0530

    a random colour palette generator with rust

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,14 @@
-#[derive(Debug)]
-enum RobotState {
-    Idle,
-    Moving, 
-    Charging,
-}
-
-pub struct Robot {
-    pub id: u32,
-    pub name: String,
-    pub x: i32,
-    pub y: i32,
-    pub battery: i32,
-    pub state: RobotState,
-}
-
-pub struct World {
-    pub robots: Vec<Robot>,
-}
-
-impl World {
-    pub fn tick(&mut self) {
-        for robot in &mut self.robots {
-            match robot.state {
-                RobotState::Charging => {
-                    robot.battery += 3;
-
-                    if robot.battery >= 100 {
-                        robot.battery = 100;
-                        robot.state = RobotState::Idle;
-                    }
-                }
-                RobotState::Moving => {
-                    robot.battery -= 2;
-                    robot.x += 1;
-
-                    if robot.battery <= 20 {
-                        robot.state = RobotState::Charging;
-                    }
-                }
-                RobotState::Idle => {
-                    robot.battery -= 1;
-
-                    if robot.battery <= 50 {
-                        robot.state = RobotState::Moving;
-                    }
-                }
-            }
-        }
-    }
-}
+use rand;
 
 fn main() {
-    let mut world = World {
-        robots: vec![
-            Robot {
-                id: 1,
-                name: String::from("Atlas"),
-                x: 0,
-                y: 0,
-                battery: 100,
-                state: RobotState::Charging,
-            }
-        ],
-    };
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
-    for tick in 0..5 {
-        println!("--- Tick {} ---", tick);
-
-        for robot in &world.robots {
-            println!(
-                "{} | {:?} | battery: {} | position: ({}, {})", 
-                robot.name,
-                robot.state,
-                robot.battery,
-                robot.x,
-                robot.y
-            );
-        }
-
-        world.tick();
+    for _i in 0..5 {
+        let mut code = String::from('#');
+        for _i in 0..6 {
+            let num = rand::random_range(0..=15);
+            code.push(hex[num]);
+        };
+        println!("{}", code)
     }
 }
